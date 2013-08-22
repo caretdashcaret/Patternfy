06341c4c593ce47f6375e04e10133953bcc338b9
Jenny <caretdashcaret@gmail.com>
2013-08-22 17:06:40 -0400

    removed scipy from transform.py

--- transform.py
+++ transform.py
@@ -1,7 +1,4 @@
-from PIL import Image
-from scipy import ndimage
-from scipy import misc
-from scipy import nditer
+import Image
 from rectransform import *
 import ImageDraw
 
@@ -22,7 +19,6 @@ def transform_img(channel, original_face_to_vts, original_vts, mod_face_to_vts, 
         fy = [int((-1*float(y[1]))*height) for y in pts]
 
         fp = zip(fx, fy)
-        #print fp
         m_set = mod_face_to_vts[face_idx]
 
         mpts = [mod_vts[int(i)-1] for i in m_set] 
@@ -31,13 +27,9 @@ def transform_img(channel, original_face_to_vts, original_vts, mod_face_to_vts, 
         ty = [int((1-float(y[1]))*height) for y in mpts]
 
         tp = zip(tx,ty)
-        #print tp
 
-        #if face_idx==1:
         rec_transform(fp, tp, channel, new_image)
-        #break
-        
-        
+                
         face_idx += 1
 
     return new_image
