f48bc62996890a3d0c2c799d5cff43194573482a
Jenny <caretdashcaret@gmail.com>
2014-03-27 12:26:00 -0400

    Add auto-compute of factors.

--- transform.py
+++ transform.py
@@ -9,6 +9,32 @@ def transform_img(channel, original_face_to_vts, original_vts, mod_face_to_vts, 
     new_width, new_height = find_new_image_size(width, height, factor_w, factor_h)    
     new_image = Image.new("RGB", (new_width, new_height), "white")
 
+    return new_image
+
+def map_within_range(number):
+    return number - numpy.floor(number)
+
+def find_min_max(vts):
+    y_coords = [vt[1] for vt in vts]
+    x_coords = [vt[0] for vt in vts]
+
+    return max(x_coords) - min(x_coords), max(y_coords) - min(y_coords)
+
+def find_scaling_factors(original_vts, modified_vts):
+    orignal_vts_width, original_vts_height = find_min_max(original_vts)
+    modified_vts_width, modified_vts_height = find_min_max(modified_vts)
+
+    factor_w = numpy.ceil(modified_vts_width / orignal_vts_width)
+    factor_h = numpy.ceil(modified_vts_height / original_vts_height)
+
+    return factor_w, factor_h
+
+def find_new_image_size(original_width, original_height, factor_w, factor_h):
+    new_width = int(original_width * factor_w)
+    new_height = int(original_height * factor_h)
+    return new_width, new_height
+
+def transform_faces(original_face_to_vts, mod_face_to_vts, channel, new_image):
     faces = len(original_face_to_vts)
     face_idx = 0
 
@@ -34,28 +60,3 @@ def transform_img(channel, original_face_to_vts, original_vts, mod_face_to_vts, 
         rec_transform(fp, tp, channel, new_image)
                 
         face_idx += 1
-
-    return new_image
-
-def map_within_range(number):
-    return number - numpy.floor(number)
-
-def find_min_max(vts):
-    y_coords = [vt[1] for vt in vts]
-    x_coords = [vt[0] for vt in vts]
-
-    return max(x_coords) - min(x_coords), max(y_coords) - min(y_coords)
-
-def find_scaling_factors(original_vts, modified_vts):
-    orignal_vts_width, original_vts_height = find_min_max(original_vts)
-    modified_vts_width, modified_vts_height = find_min_max(modified_vts)
-
-    factor_w = numpy.ceil(modified_vts_width / orignal_vts_width)
-    factor_h = numpy.ceil(modified_vts_height / original_vts_height)
-
-    return factor_w, factor_h
-
-def find_new_image_size(original_width, original_height, factor_w, factor_h):
-    new_width = int(original_width * factor_w)
-    new_height = int(original_height * factor_h)
-    return new_width, new_height
