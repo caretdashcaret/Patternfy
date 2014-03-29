41e0a4f4833fe8b4c5e35c39dc9a2cf74e9efdf5
Jenny <caretdashcaret@gmail.com>
2014-03-29 11:02:19 -0400

    Refactor rectransform.

--- rectransform.py
+++ rectransform.py
@@ -5,34 +5,7 @@ import numpy
 #transformation
 #most of the code copied from StackOverflow, with modifications
 def transformblit(src_tri, dst_tri, src_img, dst_img):
-    ((x11,x12), (x21,x22), (x31,x32)) = src_tri
-    ((y11,y12), (y21,y22), (y31,y32)) = dst_tri
-
-    M = numpy.array([
-                     [y11, y12, 1, 0, 0, 0],
-                     [y21, y22, 1, 0, 0, 0],
-                     [y31, y32, 1, 0, 0, 0],
-                     [0, 0, 0, y11, y12, 1],
-                     [0, 0, 0, y21, y22, 1],
-                     [0, 0, 0, y31, y32, 1]
-                ])
-
-    y = numpy.array([x11, x21, x31, x12, x22, x32])
-
-    #try-catch is to prevent singular matrices
-    try:
-        A = numpy.linalg.solve(M, y)
-    except:
-        M = numpy.array([
-                     [y11, y12+1, 1, 0, 0, 0],
-                     [y21, y22-1, 1, 0, 0, 0],
-                     [y31, y32, 1, 0, 0, 0],
-                     [0, 0, 0, y11, y12+1, 1],
-                     [0, 0, 0, y21, y22-1, 1],
-                     [0, 0, 0, y31, y32, 1]
-                ])
-        y = numpy.array([x11, x21, x31, x12, x22, x32])
-        A = numpy.linalg.solve(M, y)
+    A = compute_transformation_matrix(src_tri, dst_tri)
 
     src_copy = src_img.copy()
     srcdraw = ImageDraw.Draw(src_copy)
@@ -51,7 +24,39 @@ def transformblit(src_tri, dst_tri, src_img, dst_img):
     #paste final transformed image
     dst_img.paste(transformed, mask=mask)
 
-#faces are split into triangles for transformation
+def compute_transformation_matrix(source_triangle, destination_triangle):
+    ((x11,x12), (x21,x22), (x31,x32)) = source_triangle
+    ((y11,y12), (y21,y22), (y31,y32)) = destination_triangle
+
+    M = numpy.array([
+                     [y11, y12, 1, 0, 0, 0],
+                     [y21, y22, 1, 0, 0, 0],
+                     [y31, y32, 1, 0, 0, 0],
+                     [0, 0, 0, y11, y12, 1],
+                     [0, 0, 0, y21, y22, 1],
+                     [0, 0, 0, y31, y32, 1]
+                ])
+
+    y = numpy.array([x11, x21, x31, x12, x22, x32])
+
+    #try-catch is to prevent singular matrices, it's a hack solution to just shift pixels by 1
+    #singular matrices can occur because the point coordinates are mapped to the nearest integer pixel
+    try:
+        A = numpy.linalg.solve(M, y)
+    except:
+        M = numpy.array([
+                     [y11, y12+1, 1, 0, 0, 0],
+                     [y21, y22-1, 1, 0, 0, 0],
+                     [y31, y32, 1, 0, 0, 0],
+                     [0, 0, 0, y11, y12+1, 1],
+                     [0, 0, 0, y21, y22-1, 1],
+                     [0, 0, 0, y31, y32, 1]
+                ])
+        y = numpy.array([x11, x21, x31, x12, x22, x32])
+        A = numpy.linalg.solve(M, y)
+    return A
+
+#faces are split into triangles for transformation, only handles for faces up to 4 points
 def rec_transform(src_tri, dst_tri, src_img, dst_img):
     tri11 = src_tri[0:3]
     tri21 = dst_tri[0:3]
